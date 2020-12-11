764499460b874cfcdd636b79bdd79fc4394252c9
Ravichandran <rav.adonis@gmail.com>
2020-12-11 19:13:28 +0530

    Update provider.tf

--- gcp/basic/provider.tf
+++ gcp/basic/provider.tf
@@ -1,6 +1,5 @@
 ## Configure the Google Cloud Provider
 provider "google" {
-  credentials   = file("gcp_key.json")
   project       = "playground-s-11-e5d191ad"
-  region        = "asia-south1"
+  region        = "us-central1"
   }
