d0933159a5911057a7117aaa6d61ff6210eaa467
Ravichandran Somasundaram <rav.adonis@gmail.com>
2021-01-21 09:12:56 +0000

    Update Service Catalog Terraform: poc

---
+++ poc/default/terraform/module-poc/config.tf
@@ -0,0 +1,25 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "us-east-1"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
