a803d295acad3693775684a22632a9f42814a19e
sungki96 <30778193+sungki96@users.noreply.github.com>
2021-02-01 17:56:00 +0530

    Update main.tf

--- demo/scenario_02/main.tf
+++ demo/scenario_02/main.tf
@@ -113,7 +113,7 @@
  }
  # Create a SQL Database and DB instance
  resource "google_sql_database_instance" "db_instance" {
- name = "postgresmaster2"
+ name = "postgresmaster3"
  database_version = "POSTGRES_11" 
  
  settings { 
