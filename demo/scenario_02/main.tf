c2668bf681dd166ec78463eb639302979faba793
sungki96 <30778193+sungki96@users.noreply.github.com>
2021-02-02 17:27:33 +0530

    Update main.tf

--- demo/scenario_02/main.tf
+++ demo/scenario_02/main.tf
@@ -117,7 +117,7 @@
  }
  # Create a SQL Database and DB instance
  resource "google_sql_database_instance" "db_instance" {
- name = "postgresmaster7"
+ name = "postgresmaster8"
  database_version = "POSTGRES_11" 
  
   timeouts {
