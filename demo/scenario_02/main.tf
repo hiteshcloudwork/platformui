528c485f46208eb1378b413a65ac909d0beb7bcc
sungki96 <30778193+sungki96@users.noreply.github.com>
2021-02-04 13:21:01 +0530

    Update main.tf

--- demo/scenario_02/main.tf
+++ demo/scenario_02/main.tf
@@ -117,7 +117,7 @@
  #}
  # Create a SQL Database and DB instance
  resource "google_sql_database_instance" "db_instance" {
- name = "postgresmaster11"
+ name = "postgresmaster12"
  database_version = "POSTGRES_11" 
  
   timeouts {
@@ -148,3 +148,26 @@
  host = "*"
  password = random_password.password.result
  }
+#Forwarding rule for External Network Load Balancing using Backend Services
+resource "google_compute_forwarding_rule" "t-google-compute-forwarding-rule" {
+  name                  = "cloud-platform-ui-website-forwarding-rule"
+  region                = "us-central1"
+  port_range            = 80
+  backend_service       = google_compute_region_backend_service.t-google-compute-region-backend-service.id
+}
+resource "google_compute_region_backend_service" "t-google-compute-region-backend-service" {
+  name                  = "cloud-platform-ui-website-backend"
+  region                = "us-central1"
+  load_balancing_scheme = "EXTERNAL"
+  health_checks         = [google_compute_region_health_check.t-google-compute-region-health-check.id]
+}
+resource "google_compute_region_health_check" "t-google-compute-region-health-check" {
+  name               = "cloud-platform-ui-check-website-backend"
+  check_interval_sec = 1
+  timeout_sec        = 1
+  region             = "us-central1"
+
+  tcp_health_check {
+    port = "80"
+  }
+}
