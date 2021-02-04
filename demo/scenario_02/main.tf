e7d7569e2668546c6814c66de2bc5321147c0f63
sungki96 <30778193+sungki96@users.noreply.github.com>
2021-02-04 17:17:28 +0530

    Update main.tf

--- demo/scenario_02/main.tf
+++ demo/scenario_02/main.tf
@@ -117,7 +117,7 @@
  #}
  # Create a SQL Database and DB instance
  resource "google_sql_database_instance" "db_instance" {
- name = "postgresmaster15"
+ name = "postgresmaster16"
  database_version = "POSTGRES_11" 
  
   timeouts {
